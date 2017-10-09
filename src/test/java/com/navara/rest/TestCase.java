5e7a5e7067b7c039de7d88d40bd35bdaebc9230d
Alex Navara <alexandr.navara@gmail.com>
2017-10-09 18:29:05 +0300

    add sample test

---
+++ src/test/java/com/navara/rest/TestCase.java
@@ -0,0 +1,14 @@
+package com.navara.rest;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestCase {
+
+    @Test
+    public void test001()
+    {
+        Assert.assertTrue(true);
+    }
+
+}
