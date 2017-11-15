1bd7916696d4a0cf63fe1a45fbc09d86ef4e684b
alex <alexandr.navara@gmail.com>
2017-11-16 00:04:55 +0200

    Add maths servlet

---
+++ WebServletSample/src/main/java/com/example/MathServlet.java
@@ -0,0 +1,34 @@
+package com.example;
+
+import com.example.models.ParseExpressionException;
+import com.example.services.Calculator;
+import com.example.services.maths.MathService;
+import com.example.services.parser.ExpressionParser;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MathServlet extends HttpServlet {
+
+    private Calculator calculator = new Calculator(
+            new ExpressionParser(),
+            new MathService()
+    );
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String expression = req.getQueryString();
+
+        try {
+            resp.getOutputStream().println(expression + " = " + calculator.evaluate(expression));
+        } catch (ParseExpressionException e) {
+            resp.sendError(400, e.getMessage());
+        }
+
+    }
+}
