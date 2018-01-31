6626148e7b224c971cf37bfacc77454b555a4d5f
alex <alexandr.navara@gmail.com>
2018-01-31 18:53:14 +0200

    Add apache api client implementation Apply benchmark rule in tests

--- ApiTests/src/main/java/com/example/api/ApacheClientImplementation.java
+++ ApiTests/src/main/java/com/example/api/ApacheClientImplementation.java
@@ -1,43 +1,80 @@
 package com.example.api;
 
 import com.example.config.ConfigProvider;
+import com.example.models.Equation;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Type;
 import java.net.URI;
+import java.util.Collection;
+import java.util.List;
 
-public final class ApacheClientImplementation {
-//    @Override
-    public String evaluateExpression(String expression) {
+public final class ApacheClientImplementation implements MathApiClient {
 
-        final HttpUriRequest request =
-                new HttpGet(URI.create(ConfigProvider.INSTANCE.getServerUrl() + "/Calculator?" + expression.replaceAll(" ", "")));
-        final StringBuilder result = new StringBuilder();
+    private static final String BASE_URI = ConfigProvider.INSTANCE.getServerUrl() + "/Calculator";
 
-        try (CloseableHttpResponse response =
-                     HttpClientBuilder.create().build().execute(request)) {
-            final int statusCode = response.getStatusLine().getStatusCode();
+    @Override
+    public Equation postEquation(Equation equation) {
 
-            if (statusCode == 200) {
-                try (BufferedReader in = new BufferedReader(new InputStreamReader(response.getEntity().getContent()))) {
-                    String line;
-                    while ((line = in.readLine()) != null) {
-                        result.append(line);
-                    }
-                }
-            } else {
-                throw new RuntimeException("Server returned " + statusCode + " code");
+        URI uri = URI.create(BASE_URI);
+
+        final HttpPost request = new HttpPost(uri);
+        request.setEntity(new StringEntity(new Gson().toJson(equation), ContentType.APPLICATION_JSON));
+
+        return processRequest(request, Equation.class);
+    }
+
+    @Override
+    public Equation getEquationById(int id) {
+
+        URI uri = URI.create(BASE_URI + "/" + id);
+
+        final HttpGet request = new HttpGet(uri);
+
+        return processRequest(request, Equation.class);
+    }
+
+    @Override
+    public List<Equation> getAllEquations() {
+        return processRequest(new HttpGet(BASE_URI), getCollectionType(Equation.class));
+    }
+
+    @Override
+    public void deleteEquation(int id) {
+        processRequest(new HttpDelete(BASE_URI + "/" + id), null);
+    }
+
+    private <T> T processRequest(final HttpRequestBase request, final Type clazz)
+    {
+        try (CloseableHttpResponse response = HttpClientBuilder.create().build().execute(request)){
+            if (response.getStatusLine().getStatusCode() != 200) {
+                return null;
             }
 
+            if (response.getEntity().getContentLength() > 0) {
+                return new Gson().fromJson(new InputStreamReader(response.getEntity().getContent()), clazz);
+            } else {
+                return null;
+            }
         } catch (IOException e) {
             e.printStackTrace();
+            throw new RuntimeException(e);
         }
+    }
 
-        return result.toString();
+    private static Type getCollectionType(final Class elementType)
+    {
+        return TypeToken.getParameterized(Collection.class, elementType).getType();
     }
 }
--- ApiTests/src/test/java/com/example/CalculatorApiTest.java
+++ ApiTests/src/test/java/com/example/CalculatorApiTest.java
@@ -1,17 +1,21 @@
 package com.example;
 
 import com.example.api.ApacheClientImplementation;
-import com.example.api.JavaNetClientImplementation;
 import com.example.api.MathApiClient;
 import com.example.api.RestAssuredClientImplementation;
 import com.example.models.Equation;
+import com.example.rules.BenchmarkRule;
 import org.junit.Assert;
 import org.junit.Ignore;
+import org.junit.Rule;
 import org.junit.Test;
 
 public class CalculatorApiTest {
 
-    private MathApiClient apiClient;
+    @Rule
+    public BenchmarkRule benchmarkRule = new BenchmarkRule();
+
+    private MathApiClient apiClient = new ApacheClientImplementation();
 
     @Ignore
     @Test
@@ -20,10 +24,9 @@ public class CalculatorApiTest {
         executeTest();
     }
 
-    @Ignore
     @Test
     public void withApacheClient() {
-//        apiClient = new ApacheClientImplementation();
+        apiClient = new ApacheClientImplementation();
         executeTest();
     }
 
@@ -33,12 +36,26 @@ public class CalculatorApiTest {
         executeTest();
     }
 
+    @Test
+    public void withRestGet() {
+        apiClient = new RestAssuredClientImplementation();
+        apiClient.getAllEquations();
+    }
+
+    @Test
+    public void withApacheGet() {
+        apiClient = new ApacheClientImplementation();
+        apiClient.getAllEquations();
+    }
+
     private void executeTest() {
         final Integer expected = 42;
         final Equation equation = new Equation();
         equation.setExpression("84/2");
         final Integer actual = apiClient.postEquation(equation).getResult();
         Assert.assertEquals(expected, actual);
+
+        System.out.println(apiClient.getAllEquations().size());
     }
 
 }
